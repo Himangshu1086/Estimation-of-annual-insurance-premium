dbf9bfe6802a6659d5175ed4e24a7a993ca0f153
Himangshu1086 <baishyahimangshu499@gmail.com>
2022-07-06 01:43:57 +0530

    final website

--- website/index.js
+++ website/index.js
@@ -1,23 +1,63 @@
 // Requiring module
 const express = require("express");
+var bodyParser = require('body-parser');
 const app = express();
 const path = require("path")
 const PORT = 3000
+const tf  = require("@tensorflow/tfjs")
+
 // Set public as static directory
 app.use(express.static('public'));
-  
 app.set('views', path.join(__dirname, '/view'))
-  
+
+
+
 // Use ejs as template engine
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-  
+var bodyParserEncoded = bodyParser.urlencoded({ extended: false }); 
 
-// Render main template
+
+
+// Render home template
 app.get('/',(req,res)=>{
-    res.render('home')
+  res.render('home')
 })
+
+
+
+
+const mean_var = require("./mean_variance.json")
+// console.log(mean_var)
+
+function zScore(mean , variance , element)
+{
+    return (element - mean )/Math.sqrt(variance);
+}
+
+
+
+app.post('/predict',bodyParserEncoded, async(req, res) => {
+
+  const age_zs = zScore( mean_var.mean['0'],mean_var.variance[0],req.body.age);
+  const bmi_zs = zScore( mean_var.mean['1'],mean_var.variance[1],req.body.bmi);
+  const child_zs = zScore( mean_var.mean['2'],mean_var.variance[2],req.body.children);
+
+  const input = [age_zs,bmi_zs,child_zs, req.body.smoke , req.body.sex]
+
+ await tf.loadLayersModel('http://localhost:3000//Js_Model/model.json')
+.then(function (model) {
+ const scores =  model.predict([tf.tensor([input])]).arraySync()
+ console.log(scores[0][0])
+ res.render('predict' , {data:[req.body.age,req.body.bmi,req.body.children, req.body.smoke , req.body.sex] , result:Math.round(scores[0][0])})
+});
+
+});
+
+
+
+
 
 
 
--- website/public/script.js
+++ website/public/script.js
@@ -1,62 +1,12 @@
-
-// Setting up tfjs with the model we downloaded
-tf.loadLayersModel('Js_Model/model.json')
-    .then(function (model) {
-        window.model = model;
-        console.log(model)
-    });
-
-
-// let scaler = new dfd.StandardScaler();
-// console.log(scaler)  
-// var ele = scaler.fit([19,27.900,0])
-// console.log(ele)
-// console.log(scaler.transform(ele))
-
-
-// function getStandardDeviation (array) {
-//     const n = array.length
-//     const mean = array.reduce((a, b) => a + b) / n
-//     return Math.sqrt(array.map(x => Math.pow(x , 2)).reduce((a, b) => a + b) / n)
-// }
-
-// function mean(array){
-//     const n = array.length
-//     const mean_val = array.reduce((a, b) => a + b) / n
-//     return mean_val
-// }
-
-// const arr = [19,27.900,0]
-// const sd = getStandardDeviation([19,27.9,0])
-// const mean_val_cal = mean([19,27.9,0])
-// console.log(sd,mean_val_cal)
-
-// const zScore = (27.9-mean_val_cal)/sd;
-// const temp = []
-// for(var i =0;i<3;i=i+1)
-// {
-//     temp.push((arr[i]-mean_val_cal)/sd);
-// }
-// temp.push(1)
-// temp.push(0)
-// console.log(temp)
-
-
-
-// Predict function
-var predict = function (input) {
-    if (window.model) {
-        window.model.predict([tf.tensor(input)])
-            .array().then(function (scores) {
-              console.log(scores)
-            });
-    } else {
-  
-        // The model takes a bit to load, 
-        // if we are too fast, wait
-        setTimeout(function () { predict(input) }, 50);
-    }
+function estimate()
+{
+    document.getElementById("form_for_estimate").style.display = 'block';
 }
 
-const input = [[-1.43876426,-0.45332,-0.90861367,1,0]]
-predict([input])
+function get_bmi ()
+{
+    let wt = document.getElementById("weight").value ;
+    let ht = document.getElementById("height").value;
+    const bmi = (wt / Math.pow(ht,2))*10000;
+    document.getElementById("show_bmi").innerHTML = bmi.toFixed(2);
+}
