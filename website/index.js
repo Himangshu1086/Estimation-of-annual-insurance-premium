f2bd28cbb9c2e288ddf2b899bdc47a02f996836a
Himangshu1086 <baishyahimangshu499@gmail.com>
2022-08-24 16:57:04 +0530

    update

--- website/index.js
+++ website/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 var bodyParser = require('body-parser');
 const app = express();
 const path = require("path")
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const tf  = require("@tensorflow/tfjs")
 
 // Set public as static directory
